f4520be55f6c9e331c49edb081c8eb7c1c29731e
Harsh Vardhan <hv04090@gmail.com>
2026-08-21 15:17:31 +0530

    Fix auth

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -73,9 +73,11 @@ router.post("/login", async (req, res) => {
     );
 
     res.cookie("token", token, {
-        httpOnly: true,
-        maxAge: 24 * 60 * 60 * 1000
-    });
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000
+});
 
     res.status(200).json({
         success: true,
